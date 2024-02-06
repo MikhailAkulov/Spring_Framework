483da874a4bc42e54663e046e494af1fe14e37e5
Akuloff <akuloff.m@gmail.com>
2024-02-06 15:59:47 +0300

    updated Example 7 project

---
+++ Examples/Example_7/Tasks/my-spring-demo/src/main/java/ru/gb/myspringdemo/api/BookRequest.java
@@ -0,0 +1,9 @@
+package ru.gb.myspringdemo.api;
+
+import lombok.Data;
+
+@Data
+public class BookRequest {
+
+    private String name;
+}
---
+++ Examples/Example_7/Tasks/my-spring-demo/src/main/java/ru/gb/myspringdemo/api/ReaderRequest.java
@@ -0,0 +1,9 @@
+package ru.gb.myspringdemo.api;
+
+import lombok.Data;
+
+@Data
+public class ReaderRequest {
+
+    private String name;
+}
