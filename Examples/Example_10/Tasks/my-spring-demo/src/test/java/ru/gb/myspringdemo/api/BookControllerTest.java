9f3985b41558705cc3e8408dbc1b194007df65c4
Akuloff <akuloff.m@gmail.com>
2024-02-19 17:59:27 +0300

    updated Example 10 project

--- Examples/Example_10/Tasks/my-spring-demo/src/test/java/ru/gb/myspringdemo/api/BookControllerTest.java
+++ Examples/Example_10/Tasks/my-spring-demo/src/test/java/ru/gb/myspringdemo/api/BookControllerTest.java
@@ -6,14 +6,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.web.reactive.server.StatusAssertions;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.client.HttpClientErrorException;
 import ru.gb.myspringdemo.JUnitSpringBootBase;
 import ru.gb.myspringdemo.model.Book;
 import ru.gb.myspringdemo.repository.BookRepository;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 class BookControllerTest extends JUnitSpringBootBase {
 
@@ -63,4 +68,82 @@ class BookControllerTest extends JUnitSpringBootBase {
         }
     }
 
+    @Test
+    void testFindByIdSuccess() {
+        Book expected = bookRepository.save(new Book(1L, "Book_1"));
+
+        JUnitBookResponse responseBody = webTestClient.get()
+                .uri("/book/" + expected.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(JUnitBookResponse.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertNotNull(responseBody);
+        Assertions.assertEquals(expected.getId(), responseBody.getId());
+        Assertions.assertEquals(expected.getName(), responseBody.getName());
+    }
+
+    @Test
+    void testFindByIdNotFound() {
+        bookRepository.save(new Book(1L, "Book_1"));
+
+        Long maxId = jdbcTemplate.queryForObject("select max(id) from books", Long.class);
+        maxId++;
+
+        webTestClient.get()
+                .uri("/book/" + maxId)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testSaveBookSuccess() {
+        JUnitBookResponse request = new JUnitBookResponse();
+        request.setId(1L);
+        request.setName("Book_1");
+
+        JUnitBookResponse responseBody = webTestClient.post()
+                .uri("/book")
+                .bodyValue(request)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(JUnitBookResponse.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertNotNull(responseBody);
+        Assertions.assertNotNull(responseBody.getId());
+        Assertions.assertTrue(bookRepository.findById(request.getId()).isPresent());
+    }
+
+    @Test
+    void testDeleteBookSuccess() {
+        bookRepository.saveAll(List.of(
+                new Book(1L, "Book_1"),
+                new Book(2L, "Book_2")
+        ));
+        Long deletedId = jdbcTemplate.queryForObject("select max(id) from books", Long.class);
+
+        webTestClient.delete()
+                .uri("/book/ " + deletedId)
+                .exchange()
+                .expectStatus().isOk();
+
+        Assertions.assertFalse(bookRepository.existsById(deletedId));
+    }
+
+    @Test
+    void testDeleteBookNotFound() {
+        bookRepository.saveAll(List.of(
+                new Book(1L, "Book_1"),
+                new Book(2L, "Book_2")
+        ));
+        Long maxId = jdbcTemplate.queryForObject("select max(id) from books", Long.class);
+        maxId++;
+
+        webTestClient.delete()
+                .uri("/book/ " + maxId)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
 }
