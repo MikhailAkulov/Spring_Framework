d2f02ef9f4c9bd54b833542ea785beb22df3fe1a
Akuloff <akuloff.m@gmail.com>
2024-05-15 16:16:57 +0300

    updated security module

--- Examples/Example_7/Tasks/my-spring-demo/src/main/java/ru/gb/myspringdemo/security/SecurityConfiguration.java
+++ Examples/Example_7/Tasks/my-spring-demo/src/main/java/ru/gb/myspringdemo/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,7 +19,9 @@ public class SecurityConfiguration {
                         .requestMatchers("/ui/books/**").authenticated()
                         .requestMatchers("/ui/**").permitAll()
                         .anyRequest().permitAll()
-                ).formLogin(Customizer.withDefaults())
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
                 .build();
     }
 }
